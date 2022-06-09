c17a5869140a63f20c806f2228a4a773e993df2c
Alison-Rodrigues <alison.si1302@gmail.com>
2022-06-09 11:55:38 -0300

    Criação do projeto com Springboot

---
+++ src/main/java/senacestacaometeorologica/estacaometeorologicaapi/EstacaoMeteorologicaApiApplication.java
@@ -0,0 +1,13 @@
+package senacestacaometeorologica.estacaometeorologicaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstacaoMeteorologicaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstacaoMeteorologicaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/senacestacaometeorologica/estacaometeorologicaapi/EstacaoMeteorologicaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package senacestacaometeorologica.estacaometeorologicaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstacaoMeteorologicaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
